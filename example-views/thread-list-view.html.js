420a0eb9436927a0f2ac3535509a3db316988c20
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-02-27 16:55:39 -0500

    UI changes
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

---
+++ example-views/thread-list-view.html.js
@@ -0,0 +1,26 @@
+define(['app', '.. /cbd-forumns.js',
+    '../directives/forum-details-directive.html.js', '../directives/thread-list-directive.html.js'
+], function(app, _) {
+    app.controller("forumController", ["$scope", "$http", "$q", "$filter", "$timeout", "$location", "$route",
+        function($scope, $http, $q, $filter, $timeout, $location, $route) {
+
+            //$scope.forumId = 17384;
+
+            if (!$route.current.$$route.postUrl) {
+                throw 'Post URL not specified in route, please postUrl attribute in routes'
+            } else
+                $scope.postUrl = $route.current.$$route.postUrl;
+
+
+            if ($location.search().forumid) {
+                $scope.forumId = $location.search().forumid;
+            } else {
+                $scope.forumId = $route.current.$$route.forumId
+            }
+
+            if (!$scope.forumId)
+                $scope.forumId = 17384;
+            console.log($route.current.$$route)
+        }
+    ]);
+});
