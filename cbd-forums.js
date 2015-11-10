dd741636ae71839f51f4f2c94386e8a40b12645b
matthew dias <matthew.dias@cbd.int>
2015-11-10 15:57:20 -0500

    added pptx format

--- cbd-forums.js
+++ cbd-forums.js
@@ -81,6 +81,7 @@ app.directive('kmUpload', function($q) {
                     "application/vnd.ms-excel",
                     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                     "application/vnd.ms-powerpoint",
+                    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
                     "application/msword",
                     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
                 ]
